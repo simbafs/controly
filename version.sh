7b85d30258444749c142a5ea718c0e0e2493c7a4
SimbaFs <simba.fs@gmail.com>
2025-07-21 11:16:42 +0800

    feat(ci): Update Action Wrokflow and add a version.sh

---
+++ version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+version=$1
+
+if [ -z "$version" ]; then
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+
+# Check if the version is already set in the file
+if git tag | grep -q "$version"; then
+  echo "Version $version already exists."
+  exit 1
+fi
+
+git tag "$version"
+cd sdk && npm version "$version"
+
+git add .
+git commit -m "Release version $version"
